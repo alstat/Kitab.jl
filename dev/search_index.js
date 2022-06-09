1524ce9486904f95e2e33a10fa93e7436ecbca26
Documenter.jl <documenter@juliadocs.github.io>
2022-06-09 06:47:48 +0000

    build based on f168de6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Kitab.jl","page":"Home","title":"Kitab.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Kitab.jl","category":"page"}]
+}
